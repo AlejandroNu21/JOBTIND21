54ec2d0142a3e497a37eda8479a41395431ac7d8
EduardoVasquez21 <78975314+EduardoVasquez21@users.noreply.github.com>
2021-10-11 13:21:54 -0600

    JOBTINDInit

---
+++ JOBTIND21/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JOBTIND21.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+      : base(options)
+        {
+
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Empresa> Empresas { get; set; }
+        public DbSet<Anuncio> Anuncios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ JOBTIND21/Dominio/Anuncio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JOBTIND21.Dominio
+{
+    public class Anuncio
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id_Anuncio { get; set; }
+        public int Id_Empresa { get; set; }
+        public int Id_Usuario { get; set; }
+        public string Announcement { get; set; }
+
+        public Usuario Usuario { get; set; }
+
+        public Empresa Empresa { get; set; }
+    }
+}
